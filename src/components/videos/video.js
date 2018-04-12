90d782dd34ba14b47572da523d0a848469582ef7
robn <robn@wpcpp.com>
2018-04-12 07:27:09 -0500

    Add big play button

--- src/components/videos/video.js
+++ src/components/videos/video.js
@@ -6,7 +6,12 @@ import Title from '@youversion/melos/dist/components/typography/Heading1'
 import Body from '@youversion/melos/dist/components/typography/Body'
 import { videoDetailsJson } from '../../api/video-api-mock'
 
+const VIDEO_PLAYING = 0
+const VIDEO_PAUSED = 1
+const VIDEO_STOPPED = 2
+
 class VideoContainer extends React.Component {
+
 
 	static findBestThumbnailForVideoPoster(videoData) {
 		let poster = '', currentHeight = 0, currentWidth = 0
@@ -25,7 +30,7 @@ class VideoContainer extends React.Component {
 		super(props)
 		this.state = {
 			video: {},
-			videoIsPlaying: false
+			playbackState: VIDEO_STOPPED
 		}
 		this.videoRef = React.createRef()
 		this.playVideo = this.playVideo.bind(this)
@@ -43,59 +48,34 @@ class VideoContainer extends React.Component {
 	}
 
 	playVideo() {
-		this.setState({ videoIsPlaying: true })
+		this.setState({ playbackState: VIDEO_PLAYING })
 		this.videoRef.current.play()
 	}
 
 	pauseVideo() {
-		if (this.state.videoIsPlaying) {
+		if (this.state.playbackState === VIDEO_PLAYING) {
 			this.videoRef.current.pause()
-			this.setState({ videoIsPlaying: false })
+			this.setState({ playbackState: VIDEO_PAUSED })
 		}
 	}
 
 	render() {
-		const { video, videoIsPlaying } = this.state
+		const { video } = this.state
 		return (
 			<div>
 				<Helmet>
 					<title>{video.title} - {video.credits} | Videos | The Bible App | Bible.com</title>
 					<meta name="description" content={`${video.title} - ${video.credits} | Videos`} />
 				</Helmet>
-
 				<Card>
 					<Title>{video.title}</Title>
-					<video ref={this.videoRef} height={500} width={500} controls poster={video.poster}>
-						{video.renditions.map((rendition) => {
-							return <source key={`vr-${rendition.url}`}	src={rendition.url} type={`video/${rendition.format}`} />
-						})}
-						<track src="" />
-					</video>
-					<VideoControls {...this} videoIsPlaying={videoIsPlaying} />
+					<Video {...this} {...this.state} />
+					<VideoControls {...this } {...this.state} />
 					<Body>{video.publisher.description}</Body>
 				</Card>
 			</div>
 		)
 	}
-}
-
-const VideoControls = ({ playVideo, pauseVideo, videoIsPlaying }) => {
-	const playPause = videoIsPlaying ? pauseVideo : playVideo
-	return (
-		<div>
-			<button id="playpause" type="button" onClick={playPause}>Play/Pause</button>
-			<button id="stop" type="button" data-state="stop">Stop</button>
-			<div class="progress">
-				<progress id="progress" value="0" min="0">
-					<span id="progress-bar"></span>
-				</progress>
-			</div>
-			<button id="mute" type="button" data-state="mute">Mute/Unmute</button>
-			<button id="volinc" type="button" data-state="volup">Vol+</button>
-			<button id="voldec" type="button" data-state="voldown">Vol-</button>
-			<button id="fs" type="button" data-state="go-fullscreen">Fullscreen</button>
-		</div>
-	)
 }
 
 VideoContainer.propTypes = {
@@ -106,8 +86,75 @@ VideoContainer.propTypes = {
 	}).isRequired
 }
 
+
+
+const videoStyles = {
+	videoContainer: {
+		position: 'relative',
+		height: 'auto',
+		width: '100%'
+	},
+	bigPlayButton: {
+		position: 'absolute',
+		top: '45%',
+		left: '45%',
+		zIndex: '1',
+		padding: '2em',
+		borderRadius: '1em'
+	},
+	video: {
+		width: '100%'
+	}
+}
+
+const Video = ({
+	playVideo, playbackState, video, videoRef
+}) => {
+	const bigPlayButton = playbackState === VIDEO_STOPPED ?
+		<button onClick={playVideo} style={videoStyles.bigPlayButton} type="button" aria-label="play video">Play</button> : ''
+	return (
+		<div style={videoStyles.videoContainer}>
+			{bigPlayButton}
+			<video style={videoStyles.video} ref={videoRef} poster={video.poster}>
+				{video.renditions.map((rendition) => {
+					return <source key={`vr-${rendition.url}`}	src={rendition.url} type={`video/${rendition.format}`} />
+				})}
+				<track src="" />
+			</video>
+		</div>
+	)
+}
+
+Video.propTypes = {
+	playbackState: PropTypes.number.isRequired,
+	playVideo: PropTypes.func.isRequired,
+	videoRef: PropTypes.string.isRequired,
+	video: PropTypes.objectOf({
+		poster: PropTypes.string.isRequired,
+		reditions: PropTypes.array.isRequired
+	}).isRequired
+}
+
+const VideoControls = ({ playVideo, pauseVideo, playbackState }) => {
+	const playPause = playbackState === VIDEO_PLAYING ? pauseVideo : playVideo
+	return (
+		<div>
+			<button id="playpause" type="button" onClick={playPause}>Play/Pause</button>
+			<div className="progress">
+				<progress id="progress" value="0" min="0">
+					<span id="progress-bar" />
+				</progress>
+			</div>
+			<button id="mute" type="button" data-state="mute">Mute/Unmute</button>
+			<button id="volinc" type="button" data-state="volup">Vol+</button>
+			<button id="voldec" type="button" data-state="voldown">Vol-</button>
+			<button id="fs" type="button" data-state="go-fullscreen">Fullscreen</button>
+		</div>
+	)
+}
+
 VideoControls.propTypes = {
-	videoIsPlaying: PropTypes.bool.isRequired,
+	playbackState: PropTypes.number.isRequired,
 	playVideo: PropTypes.func.isRequired,
 	pauseVideo: PropTypes.func.isRequired
 }
